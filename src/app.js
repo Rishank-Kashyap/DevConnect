e75cd2906f9e0997006a6235204a5d90fca0da6f
Rishank-Kashyap <rishankkashyap03@gmail.com>
2025-03-08 23:19:39 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const serverless = require("serverless-http");
 const connectDB = require("./config/databases");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -11,7 +10,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
@@ -32,14 +31,10 @@ app.use("/", userRouter);
 connectDB()
   .then(() => {
     console.log("Database connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is listening at port ${process.env.PORT}`);
+    });
   })
   .catch((err) => {
     console.log("Database cannot  be Connected");
   });
-
-  app.get("/", (req, res) => {
-    res.send("🚀 Server is running on Vercel!");
-  });
-
-  module.exports = app;
-  module.exports.handler = serverless(app);
